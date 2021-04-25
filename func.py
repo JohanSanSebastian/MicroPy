06e88d6f9b7ba06e9d4052815848075f13d97e8d
Johan Sebastian <65955878+JohanSanSebastian@users.noreply.github.com>
2021-04-25 16:29:14 +0400

    Update func.py

--- func.py
+++ func.py
@@ -1,21 +1,22 @@
 import os
 
+# All the various functions that call the AHK files
 
-def startMeetAHK():
+def startMeetAHK(): # For Google Meet
     os.startfile(
         "AHK files\\meet.ahk")
 
 
-def startTeamsAHK():
+def startTeamsAHK(): # For Teams
     os.startfile(
         "AHK files\\teams.ahk")
 
 
-def startZoomAHK():
+def startZoomAHK(): # For Zoom
     os.startfile(
         "AHK files\\zoom.ahk")
 
 
-def startDiscordAHK():
+def startDiscordAHK(): # For Discord
     os.startfile(
         "AHK files\\discord.ahk")
